c9674d4f858aceda794025e5cc13f2f231e70f87
msnkhnn <msnkhnn@gmail.com>
2024-12-08 13:22:01 +0530

    it use bring the code in  matlab env and simulink env

---
+++ MyBlocks/SFuntion/LegacyCode.m
@@ -0,0 +1,22 @@
+mex -setup 
+% chech the complire use for ready To choose a different language, select one from the following:
+%mex -setup C++ 
+%mex -setup FORTRAN
+
+def = legacy_code('initialize')
+
+
+ SFunctionName: ''
+    def.InitializeConditionsFcnSpec: ''
+                  def.OutputFcnSpec: ''
+                   def.StartFcnSpec: ''
+               def.TerminateFcnSpec: ''
+                    def.HeaderFiles: {}
+                    def.SourceFiles: {}
+                   def.HostLibFiles: {}
+                 def.TargetLibFiles: {}
+                       def.IncPaths: {}
+                       def.SrcPaths: {}
+                       def.LibPaths: {}
+                     def.SampleTime: 'inherited'
+                        Options: [1×1 struct]
